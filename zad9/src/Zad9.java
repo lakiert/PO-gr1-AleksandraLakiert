57878407175eda7d6eabd22e7acfc807411afe98
lakiert <73653530+lakiert@users.noreply.github.com>
2021-10-06 23:47:59 +0200

    zad9

---
+++ zad9/src/Zad9.java
@@ -0,0 +1,10 @@
+public class Zad9 {
+    public static void main(String[] args) {
+        System.out.println(
+                "            -----\n" +
+                " /\\_/\\    / Whats \\\n" +
+                "( '-' )  <   Up    |\n" +
+                "|  -  |   \\ Dude! /\n" +
+                "(__|__)     -----");
+    }
+}
