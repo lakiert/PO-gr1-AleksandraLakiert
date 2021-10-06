3b5187ba61cf978588391e17bd0fc81c3f471860
lakiert <73653530+lakiert@users.noreply.github.com>
2021-10-06 23:38:42 +0200

    zad7

---
+++ zad7/src/Zad7.java
@@ -0,0 +1,11 @@
+public class Zad7 {
+    public static void main(String[] args) {
+        System.out.println(
+                "\n"+
+                " ****    *       **** \n"+
+                "*    *   *      *    *\n"+
+                "*    *   *      *    *\n"+
+                "*    *   *      ******\n"+
+                " ****    *****  *    *\n");
+    }
+}
