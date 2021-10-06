5d81f984d0ffb377afa3a19a444cfdcc9884c071
lakiert <73653530+lakiert@users.noreply.github.com>
2021-10-06 23:55:18 +0200

    zad11

---
+++ zad11/src/Zad11.java
@@ -0,0 +1,37 @@
+public class Zad11 {
+    public static void main(String[] args) {
+        System.out.println("Wisława Szymborska, \"Cebula\"\n" +
+                "Co innego cebula.\n" +
+                "Ona nie ma wnętrzności.\n" +
+                "Jest sobą na wskroś cebulą,\n" +
+                "do stopnia cebuliczności.\n" +
+                "Cebulasta na zewnątrz,\n" +
+                "cebulowa do rdzenia,\n" +
+                "mogłaby wejrzeć w siebie\n" +
+                "cebula bez przerażenia.\n" +
+                "W nas obczyzna i dzikość\n" +
+                "ledwie skórą przykryta,\n" +
+                "inferno w nas interny,\n" +
+                "anatomia gwałtowna,\n" +
+                "a w cebuli cebula,\n" +
+                "nie pokrętne jelita.\n" +
+                "Ona wielekroć naga,\n" +
+                "do głębi itympodobna.\n" +
+                "Byt niesprzeczny cebula,\n" +
+                "udany cebula twór.\n" +
+                "W jednej po prostu druga,\n" +
+                "w większej mniejsza zawarta,\n" +
+                "a w następnej kolejna,\n" +
+                "czyli trzecia i czwarta.\n" +
+                "Dośrodkowa fuga.\n" +
+                "Echo złożone w chór.\n" +
+                "Cebula, to ja rozumiem:\n" +
+                "najnadobniejszy brzuch świata.\n" +
+                "Sam się aureolami\n" +
+                "na własną chwałę oplata.\n" +
+                "W nas - tłuszcze, nerwy, żyły,\n" +
+                "śluzy i sekretności.\n" +
+                "I jest nam odmówiony\n" +
+                "idiotyzm doskonałości.");
+    }
+}
