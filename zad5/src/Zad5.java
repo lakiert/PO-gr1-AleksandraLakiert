c200c107d8579ac6d0e4ab9c7e2766c7993e9c78
lakiert <73653530+lakiert@users.noreply.github.com>
2021-10-13 11:18:00 +0200

    Delete zad5 directory

--- zad5/src/Zad5.java
+++
@@ -1,7 +0,0 @@
-public class Zad5 {
-    public static void main(String[] args) {
-        System.out.println("+------+");
-        System.out.println("| JAVA |");
-        System.out.println("+------+");
-    }
-}
