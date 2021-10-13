350ec9855f61d916016d5e14b07b46957101743d
lakiert <73653530+lakiert@users.noreply.github.com>
2021-10-13 11:16:27 +0200

    Add files via upload

---
+++ lab1/zad10/src/Zad10.java
@@ -0,0 +1,7 @@
+public class Zad10 {
+    public static void main(String[] args) {
+        System.out.println("\"Matrix\" \n" +
+                "\"Piraci z Karaibów\" \n" +
+                "\"Władca Pierścieni\"");
+    }
+}
---
+++ lab1/zad3/src/Zad3.java
@@ -0,0 +1,10 @@
+public class Zad3 {
+    public static void main(String[] args) {
+        int iloczyn=1;
+        for(int i=1; i<=10; i++)
+        {
+            iloczyn*=i;
+        }
+        System.out.println(iloczyn);
+    }
+}
