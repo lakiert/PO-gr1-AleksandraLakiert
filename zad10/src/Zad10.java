fd8502bff42fbf22f56a66670f76ca2115173114
lakiert <73653530+lakiert@users.noreply.github.com>
2021-10-13 11:17:27 +0200

    Delete zad10 directory

--- zad10/src/Zad10.java
+++
@@ -1,7 +0,0 @@
-public class Zad10 {
-    public static void main(String[] args) {
-        System.out.println("\"Matrix\" \n" +
-                "\"Piraci z Karaibów\" \n" +
-                "\"Władca Pierścieni\"");
-    }
-}
