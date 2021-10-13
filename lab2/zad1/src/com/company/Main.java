a14e5cecb9e4483b4dfaf04fdce179bf8a4eddd7
lakiert <73653530+lakiert@users.noreply.github.com>
2021-10-13 12:32:45 +0200

    Add files via upload

---
+++ lab2/zad1/src/com/company/Main.java
@@ -0,0 +1,194 @@
+package com.company;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+
+        // zad1
+
+//        System.out.println("podaj ilosc elementow: ");
+//        int n = in.nextInt();
+//        double suma=0;
+//
+//        for (int i = 1; i <= n; i++) {
+//
+//            System.out.println("podaj a" + i + ": ");
+//            double a = in.nextDouble();
+//            suma += a;
+//
+//        }
+//
+//        System.out.println("suma wynosi: " + suma);
+
+
+
+        // zad2
+
+//        System.out.println("podaj ilosc elementow: ");
+//        int n = in.nextInt();
+//        double iloczyn=1;
+//
+//        for (int i = 1; i <= n; i++) {
+//
+//            System.out.println("podaj a" + i + ": ");
+//            double a = in.nextDouble();
+//            iloczyn *= a;
+//
+//        }
+//
+//        System.out.println("iloczyn wynosi: " + iloczyn);
+
+
+        // zad3
+
+//        System.out.println("podaj ilosc elementow: ");
+//        int n = in.nextInt();
+//        double suma=0;
+//
+//        for (int i = 1; i <= n; i++) {
+//
+//            System.out.println("podaj a" + i + ": ");
+//            double a = in.nextDouble();
+//            if (a<0) a=a*-1;
+//            suma += a;
+//
+//        }
+//
+//        System.out.println("suma wynosi: " + suma);
+
+
+
+        // zad4
+
+//        System.out.println("podaj ilosc elementow: ");
+//        int n = in.nextInt();
+//        double suma=0;
+//
+//        for (int i = 1; i <= n; i++) {
+//
+//            System.out.println("podaj a" + i + ": ");
+//            double a = in.nextDouble();
+//            if (a<0) a=a*-1;
+//            suma += Math.sqrt(a);
+//
+//        }
+//
+//        System.out.println("suma wynosi: " + suma);
+
+
+        // zad5
+
+//        System.out.println("podaj ilosc elementow: ");
+//        int n = in.nextInt();
+//        double iloczyn=1;
+//
+//        for (int i = 1; i <= n; i++) {
+//
+//            System.out.println("podaj a" + i + ": ");
+//            double a = in.nextDouble();
+//            if (a<0) a=a*-1;
+//            iloczyn *= a;
+//
+//        }
+//
+//        System.out.println("iloczyn wynosi: " + iloczyn);
+
+
+
+        // zad6
+
+//        System.out.println("podaj ilosc elementow: ");
+//        int n = in.nextInt();
+//        double suma=0;
+//
+//        for (int i = 1; i <= n; i++) {
+//
+//            System.out.println("podaj a" + i + ": ");
+//            double a = in.nextDouble();
+//            suma += Math.pow(a,2);
+//
+//        }
+//
+//        System.out.println("suma wynosi: " + suma);
+
+
+
+        // zad7
+
+//        System.out.println("podaj ilosc elementow: ");
+//        int n = in.nextInt();
+//        double suma=0;
+//        double iloczyn=1;
+//
+//        for (int i = 1; i <= n; i++) {
+//
+//            System.out.println("podaj a" + i + ": ");
+//            double a = in.nextDouble();
+//            suma += a;
+//            iloczyn *= a;
+//
+//        }
+//
+//        System.out.println("suma wynosi: " + suma);
+//        System.out.println("iloczyn wynosi: " + iloczyn);
+
+
+
+        // zad8
+
+//        System.out.println("podaj ilosc elementow: ");
+//        int n = in.nextInt();
+//        double suma=0;
+//
+//        for (int i = 1; i <= n; i++) {
+//
+//            System.out.println("podaj a" + i + ": ");
+//            double a = in.nextDouble();
+//
+//            if(i%2!=0) suma += a;
+//            if(i%2==0) suma -= a;
+//
+//
+//        }
+//
+//
+//        System.out.println("suma wynosi: " + suma);
+
+
+
+        // zad9
+
+//        System.out.println("podaj ilosc elementow: ");
+//        int n = in.nextInt();
+//        double suma=0;
+//        double an=0;
+//        double silnia =1;
+//
+//        for (int i = 1; i <= n; i++) {
+//
+//            System.out.println("podaj a" + i + ": ");
+//            double a = in.nextDouble();
+//
+//            for(int j=i; j>=i; j--)
+//            {
+//                silnia *= i;
+//                System.out.println(silnia);
+//
+//                if(j%2!=0) suma -= (a/silnia);
+//                if(j%2==0) suma += (a/silnia);
+//            }
+//
+//
+//        }
+//
+//
+//        System.out.println("suma wynosi: " + suma);
+
+    }
+
+}
