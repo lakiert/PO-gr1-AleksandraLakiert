b0cfbd2d46f2283e744d4ff3350bf6436b0e5aa2
lakiert <73653530+lakiert@users.noreply.github.com>
2021-11-10 11:44:56 +0100

    Add files via upload

---
+++ lab6/src/com/company/Main.java
@@ -0,0 +1,73 @@
+package com.company;
+import java.util.ArrayList;
+
+public class Main {
+
+
+
+
+    // zad1
+
+    public static ArrayList<Integer> append(ArrayList<Integer> a, ArrayList<Integer> b)
+    {
+
+        ArrayList<Integer> nowy = new ArrayList<Integer>();
+
+        nowy.addAll(a);
+        nowy.addAll(b);
+
+        return nowy;
+
+    }
+
+
+
+    // zad2
+
+//    ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b)
+//    {
+//
+//
+//
+//    }
+
+
+
+    public static void main(String[] args) {
+
+
+        // zad1
+
+//    ArrayList<Integer> a = new ArrayList<Integer>();
+//    ArrayList<Integer> b = new ArrayList<Integer>();
+//
+//    a.add(1);
+//    a.add(4);
+//    a.add(9);
+//    a.add(16);
+//
+//    b.add(9);
+//    b.add(7);
+//    b.add(4);
+//    b.add(9);
+//    b.add(11);
+//
+//    System.out.println(a);
+//    System.out.println(b);
+//    System.out.println(append(a,b));
+
+
+
+
+
+
+        // zad2
+
+
+
+
+
+
+
+    }
+}
