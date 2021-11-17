a0a2d8c931a151d1164f657a8ee2302dc5752ffd
lakiert <73653530+lakiert@users.noreply.github.com>
2021-11-17 11:24:17 +0100

    Add files via upload

---
+++ lab7/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+
+
+class RachunekBankowy {
+
+    static double rocznaStopaProcentowa;
+    private double saldo;
+
+
+    public static double obliczMiesieczneOdsetki(double capital) {
+
+
+    return 5;
+    }
+
+    public static double setRocznaStopaProcentowa(double capital) {
+
+
+    return 6;
+    }
+}
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(RachunekBankowy.obliczMiesieczneOdsetki(2.5));
+        System.out.println(RachunekBankowy.setRocznaStopaProcentowa(2.5));
+
+    }
+}
