a746541d98cf5296ffbf4d065cad04fbafe9ae1a
lakiert <73653530+lakiert@users.noreply.github.com>
2021-11-20 01:17:17 +0100

    Update Main.java

--- lab7/src/com/company/Main.java
+++ lab7/src/com/company/Main.java
@@ -1,46 +1,37 @@
 package com.company;
 
-
-
 class RachunekBankowy {
 
-    static double rocznaStopaProcentowa;
+
+    private static double rocznaStopaProcentowa;
     private double saldo;
 
+
+    RachunekBankowy(double saldo)
+    {
+        this.saldo = saldo;
+    }
 
     public double getSaldo()
     {
         return saldo;
     }
 
-
-    public RachunekBankowy(double saldo)
+    public void setRocznaStopaProcentowa(double rocznaStopaProcentowa)
     {
-        this.saldo = saldo;
+        this.rocznaStopaProcentowa = rocznaStopaProcentowa;
+    }
+
+    public double obliczMiesieczneOdsetki(double saldo)
+    {
+        double odsetki = saldo*rocznaStopaProcentowa/12;
+        this.saldo +=odsetki;
+        return odsetki;
+
     }
 
 
-// //   public double setSaldo(double saldo)
-// //   {
-// //       saldo = this.saldo;
-// //       return saldo;
-// //   }
 
-
-
-    public static double obliczMiesieczneOdsetki(double saldo) {
-
-    double odsetki = (saldo * rocznaStopaProcentowa)/12 ;
-    saldo += odsetki;  ////////////////////////////////////////////////////
-    return odsetki;
-    }
-
-
-    public static double setRocznaStopaProcentowa(double x) {
-
-    rocznaStopaProcentowa = x;
-    return rocznaStopaProcentowa;
-    }
 
 }
 
@@ -53,10 +44,24 @@ public class Main {
         RachunekBankowy saver1 = new RachunekBankowy(2000);
         RachunekBankowy saver2 = new RachunekBankowy(3000);
 
+        System.out.println("saver1: " + saver1.getSaldo());
+        System.out.println("saver2: " + saver2.getSaldo());
 
-        System.out.println(RachunekBankowy.setRocznaStopaProcentowa(0.04));
-        System.out.println(RachunekBankowy.obliczMiesieczneOdsetki(saver1.getSaldo()));
-        System.out.println(RachunekBankowy.obliczMiesieczneOdsetki(saver2.getSaldo()));
+        System.out.println(" ");
+        System.out.println("roczna stopa procentowa:");
+        saver1.setRocznaStopaProcentowa(0.04);
+        saver2.setRocznaStopaProcentowa(0.04);
+        System.out.println(saver1.obliczMiesieczneOdsetki(saver1.getSaldo()));
+        System.out.println(saver2.obliczMiesieczneOdsetki(saver2.getSaldo()));
+        System.out.println("saver1: " + saver1.getSaldo());
+        System.out.println("saver2: " + saver2.getSaldo());
+
+        System.out.println(" ");
+        System.out.println("roczna stopa procentowa:");
+        saver1.setRocznaStopaProcentowa(0.05);
+        saver2.setRocznaStopaProcentowa(0.05);
+        System.out.println(saver1.obliczMiesieczneOdsetki(saver1.getSaldo()));
+        System.out.println(saver2.obliczMiesieczneOdsetki(saver2.getSaldo()));
         System.out.println("saver1: " + saver1.getSaldo());
         System.out.println("saver2: " + saver2.getSaldo());
 
