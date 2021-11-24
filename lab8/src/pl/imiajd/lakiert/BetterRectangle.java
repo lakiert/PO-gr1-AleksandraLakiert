fdda8f93ca353c08c0d8ee01991bb18ff3103f72
lakiert <73653530+lakiert@users.noreply.github.com>
2021-11-24 23:27:08 +0100

    Add files via upload

---
+++ lab8/src/pl/imiajd/lakiert/BetterRectangle.java
@@ -0,0 +1,11 @@
+package pl.imiajd.lakiert;
+
+public class BetterRectangle extends java.awt.Rectangle{
+
+    public BetterRectangle(int x, int y, int a, int b)
+    {
+        super.setLocation(x,y);
+        super.setSize(a,b);
+    }
+    
+}
