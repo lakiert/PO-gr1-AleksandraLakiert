904d20907fd789e89fd3a57487f299f33f1d8617
lakiert <73653530+lakiert@users.noreply.github.com>
2021-11-24 23:16:43 +0100

    Update Main.java

--- lab8/src/com/company/Main.java
+++ lab8/src/com/company/Main.java
@@ -1,23 +1,47 @@
 // https://drive.google.com/file/d/1rYu863cK4ODiOzt787mZoXPaDut2--I2/view
 
 package com.company;
-import pl.imiajd.lakiert.Punkt;
-import pl.imiajd.lakiert.NazwanyPunkt;
+import pl.imiajd.lakiert.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello");
-        Punkt pkt = new Punkt(5,8);
-        pkt.show();
+//        System.out.println("hello");
+//        Punkt pkt = new Punkt(5,8);
+//        pkt.show();
+//
+//        System.out.println(pkt.x());
+//        System.out.println(pkt.y());
+//
+//        NazwanyPunkt pkt2 = new NazwanyPunkt(15,18, "punkt_v2");
+//        pkt2.show();
 
-        System.out.println(pkt.x());
-        System.out.println(pkt.y());
 
-        NazwanyPunkt pkt2 = new NazwanyPunkt(15,18, "punkt_v2");
-        pkt2.show();
 
+//        Adres adres1 = new Adres();
+//        Adres adres2 = new Adres();
+//        adres1.Adres_z_nr("Olsztynska", "10", "15", "Olsztyn", "10-687");
+//        adres2.Adres_bez_nr("Warszawska", "5", "Krakow", "80-253");
+//        adres1.pokaz();
+//        System.out.println(" ");
+//        adres2.pokaz();
+
+
+
+//        Osoba osoba1 = new Osoba("Nowak", 1980);
+//        Student student1 = new Student("Kowalski", 1995, "medycyna");
+//        Nauczyciel nauczyciel1 = new Nauczyciel("Wozniak", 1970, 2500);
+//
+//        osoba1.pokaz();
+//        System.out.println(" ");
+//        student1.pokaz();
+//        System.out.println(" ");
+//        nauczyciel1.pokaz();
+//
+//        System.out.println("\n nazwisko: " + osoba1.getNazwisko() + ", rok urodzenia: " + osoba1.getRokUrodzenia());
+//        System.out.println("\n nazwisko: " + student1.getNazwisko() + ", rok urodzenia: " + student1.getRokUrodzenia() + ", kierunek: " + student1.getKierunek());
+//        System.out.println("\n nazwisko: " + nauczyciel1.getNazwisko() + ", rok urodzenia: " + nauczyciel1.getRokUrodzenia() + ", pensja: " + nauczyciel1.getPensja());
 
     }
 }
