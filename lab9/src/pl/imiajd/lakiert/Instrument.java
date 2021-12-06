c768f0a132f7dc9d9d3f8faa63f4431125733d0e
lakiert <73653530+lakiert@users.noreply.github.com>
2021-12-06 23:12:17 +0100

    Update Instrument.java

--- lab9/src/pl/imiajd/lakiert/Instrument.java
+++ lab9/src/pl/imiajd/lakiert/Instrument.java
@@ -6,11 +6,16 @@ public abstract class Instrument {
     String producent;
     java.time.LocalDate rokProdukcji;
 
+    public void setProducent(String producent) {
+        this.producent = producent;
+    }
+    public void setRokProdukcji(int rok, int miesiac, int dzien) {
+        this.rokProdukcji = LocalDate.of(rok, miesiac, dzien);
+    }
 
     public String getProducent() {
         return producent;
     }
-
     public java.time.LocalDate getRokProdukcji() {
         return rokProdukcji;
     }
