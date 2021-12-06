a570b6c37e8935f81c1ab1d45bbc8232f1411ede
lakiert <73653530+lakiert@users.noreply.github.com>
2021-12-06 23:11:07 +0100

    Update Main.java

--- lab9/src/com/company/Main.java
+++ lab9/src/com/company/Main.java
@@ -20,12 +20,36 @@ public class Main {
         orkiestra.add(flet);
         orkiestra.add(flet);
 
+        flet.setProducent("muzykant");
+        skrzypce.setProducent("muzykpol");
+        fortepian.setProducent("muzyx");
+
+        flet.setRokProdukcji(2020, 8,25);
+        skrzypce.setRokProdukcji(2019, 11, 5);
+        fortepian.setRokProdukcji(2021, 1, 20);
+
 
         for(int i=0; i<orkiestra.size(); i++)
         {
-          orkiestra.get(i).dzwiek();
+            orkiestra.get(i).dzwiek();
         }
 
+        System.out.println(" ");
+
+        System.out.println("Producent fletu to: " + flet.getProducent());
+        System.out.println("Producent fortepianu to: " + fortepian.getProducent());
+        System.out.println("Producent skrzypiec to: " + skrzypce.getProducent());
+
+        System.out.println(" ");
+
+        System.out.println("Rok produkcji fletu to: " + flet.getRokProdukcji());
+        System.out.println("Rok produkcji skrzypiec to: " + skrzypce.getRokProdukcji());
+        System.out.println("Rok produkcji fortepianu to: " + fortepian.getRokProdukcji());
+
+        System.out.println(" ");
+
+        System.out.println(skrzypce.getRokProdukcji().toString());
+        System.out.println(flet.getProducent().equals(fortepian.getProducent()));
 
     }
 }
