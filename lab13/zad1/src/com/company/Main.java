c2b7e10272fdc921520556a360555d06c25ec8bc
lakiert <73653530+lakiert@users.noreply.github.com>
2022-01-12 15:30:19 +0100

    Update Main.java

--- lab13/zad1/src/com/company/Main.java
+++ lab13/zad1/src/com/company/Main.java
@@ -1,8 +1,7 @@
 package com.company;
-
-
 import java.util.Iterator;
 import java.util.PriorityQueue;
+
 
 class Task implements Comparable<Task> {
 
@@ -35,26 +34,71 @@ class Task implements Comparable<Task> {
 }
 
 
+class TaskList{
+
+    PriorityQueue<Task> task_list = new PriorityQueue<>();
+
+    public TaskList() {
+        this.task_list = new PriorityQueue<>();
+    }
+
+    public void add(Task task){
+        task_list.add(task);
+    }
+
+    public int size(){
+        return task_list.size();
+    }
+
+    public void poll(){
+        task_list.poll();
+    }
+
+    public void what_now(){
+        System.out.println(task_list.peek());
+    }
+
+    public void next(){
+        task_list.poll();
+    }
+
+    public String toString(){
+        StringBuilder result = new StringBuilder();
+        PriorityQueue<Task> temp = new PriorityQueue<Task>(task_list);
+
+        Iterator<Task> it = temp.iterator();
+        while(it.hasNext()) {
+            result.append(temp.poll()).append("\n");
+        }
+        return result.toString();
+    }
+
+
+}
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        PriorityQueue<Task> my_tasks = new PriorityQueue<>();
+        TaskList my_tasks = new TaskList();
 
         my_tasks.add(new Task(1, "walk the dog"));
-        my_tasks.add(new Task(2, "homework"));
-        my_tasks.add(new Task(5, "do the dishes"));
-        my_tasks.add(new Task(3, "make groceries"));
-        my_tasks.add(new Task(2, "have fun?"));
+        my_tasks.add(new Task(3, "wash the dishes"));
+        my_tasks.add(new Task(2, "do my homework"));
+        my_tasks.add(new Task(3, "cook dinner"));
+        my_tasks.add(new Task(1, "feed the dog"));
 
-        System.out.println(my_tasks.size());
+        System.out.println(my_tasks);
 
-        Iterator<Task> it = my_tasks.iterator();
-        while(it.hasNext()){
-            System.out.println("doing the chores: " + my_tasks.poll().toString());
+        my_tasks.next();
+        System.out.println(my_tasks);
 
-        }
+        my_tasks.next();
+        System.out.println(my_tasks);
+
+
+
 
 
     }
